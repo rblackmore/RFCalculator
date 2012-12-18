f2e2a1e39d392e44ed4335a0992b504b5534007d
Ryan Blackmore <rblackmore.ryan@gmail.com>
2012-12-18 18:36:47 +1100

    first commit

---
+++ src/com/rblackmore/RFCalculator/views/Main.java
@@ -0,0 +1,24 @@
+package com.rblackmore.RFCalculator.views;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.RFCalculator.R;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: Ryan Blackmore
+ * @Date: 17/12/12
+ * @Time: 4:55 PM
+ * @Version: 1.0
+ * To change this template use File | Settings | File Templates.
+ * <p/>
+ * Description:
+ */
+public class Main extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
